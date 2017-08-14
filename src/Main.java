2e6cd9dec42309eb5f4d4ac8aee6100049ff6ff8
hugiell <femc.tadej@gmail.com>
2017-08-14 14:27:14 +0200

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Hugiell on 13. 08. 2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main.main()");
+        check();
+    }
+
+    public static void check() {
+        System.out.println("Checking...");
+    }
+
+}
